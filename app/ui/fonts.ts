507a116d9fc4cbb8026e720629875eecbf544da4
nacht-falter <johannes.bernet@posteo.de>
2024-01-21 23:43:02 +0100

    Create initial layout

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Source_Sans_3, DM_Serif_Display } from "next/font/google";
+
+export const source_sans = Source_Sans_3({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "600", "700", "900"],
+});
+export const dm_serif = DM_Serif_Display({
+  subsets: ["latin"],
+  weight: ["400"],
+});
