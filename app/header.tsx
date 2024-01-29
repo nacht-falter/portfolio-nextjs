c061a07b909a702f41a600c6e06756fce8f2bc4b
nacht-falter <johannes.bernet@posteo.de>
2024-01-29 10:31:16 +0100

    add loading animation, adjust dark/light theme

--- app/header.tsx
+++ app/header.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { montserrat } from "./ui/fonts";
 import { useEffect, useState } from "react";
+import Image from "next/image";
 
 export default function Header() {
   const [animationFinished, setAnimationFinished] = useState<boolean>(false);
@@ -10,14 +11,24 @@ export default function Header() {
   useEffect(() => {
     const timeout = setTimeout(() => {
       setAnimationFinished(true);
-    }, 5000);
+    }, 7000);
     return () => {
       clearTimeout(timeout);
     };
   }, []);
 
+  const logo = (
+    <Image
+      src="/nacht-falter_large_shadow.svg"
+      alt="nacht-falter logo"
+      width={35}
+      height={35}
+      className="inline-block align-middle logo"
+    />
+  );
+
   return (
-    <header className="flex justify-center items-center overflow-x-clip">
+    <header className="flex justify-center items-center overflow-x-clip mt-6 lg:mt-0">
       <div
         className={`${animationFinished ? "hidden" : ""} flex w-full h-0 absolute top-0 animation-cols`}
       >
@@ -28,14 +39,23 @@ export default function Header() {
         <div className="flex-1"></div>
         <div className="flex-1"></div>
       </div>
-      <Link href="/" className={`${animationFinished ? "main-title-link" : ""}`}>
+      <Link
+        href="/"
+        className={`${animationFinished ? "main-title-link" : ""}`}
+      >
         <h1
-          className={`${montserrat.className} antialiased text-3xl md:text-4xl lg:text-5xl mt-3 flex items-center`}
+          className={`${montserrat.className} antialiased flex-col lg:flex-row flex items-center`}
         >
-          <span className="main-title uppercase text-right font-extrabold border-white border-r-2">
+          <span className="lg:hidden flex items-center justify-center aspect-square p-2 pt-3 rounded-full logo-bg mx-3">
+            {logo}
+          </span>
+          <span className="main-title uppercase text-right text-3xl md:text-4xl xl:text-5xl font-extrabold">
             Johannes Bernet
           </span>
-          <span className="text-xl md:text-2xl lg:text-3xl font-extrabold text-left subtitle">
+          <span className="hidden lg:flex items-center justify-center aspect-square p-2 pt-3 rounded-full logo-bg mx-3">
+            {logo}
+          </span>
+          <span className="text-2xl md:text-3xl lg:text-4xl font-extrabold text-left subtitle">
             Software Developer
           </span>
         </h1>
