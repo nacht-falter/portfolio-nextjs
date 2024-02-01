a2bdf428620bf3be471c7d54965d6d6a51492505
nacht-falter <johannes.bernet@posteo.de>
2024-02-01 17:43:09 +0100

    adjust contact info layout

--- app/about.tsx
+++ app/about.tsx
@@ -26,39 +26,41 @@ export default async function About() {
       <p className="mt-3">
         I have a background in musicology as a researcher and teacher.
       </p>
-      <p className="mt-3 border-t border-slate-400 pt-3 pb-0">
-        Get in touch:
-        {socialLinks.map((socialLink, i) => {
-          return (
-            <Link
-              href={socialLink.url}
-              key={i}
-              className="social-link-dark h-8 w-8 inline-block align-middle text-center aspect-square rounded-full leading-7 mx-3"
-              title={socialLink.name}
-            >
-              <Image
-                src={`https://cdn.simpleicons.org/${socialLink.icon}`}
-                alt={socialLink.name}
-                width={15}
-                height={15}
-                className="inline"
-              />
-            </Link>
-          );
-        })}
-        <Link
-          href={`mailto:${CONTACT_EMAIL}`}
-          title="Contact me"
-          className="social-link-dark h-8 w-8 inline-block align-middle text-center aspect-square rounded-full leading-7 mx-3"
-        >
-          <Image
-            src="/envelope-solid.svg"
-            alt="mail"
-            width={15}
-            height={15}
-            className="inline"
-          />
-        </Link>
+      <p className="flex flex-col lg:flex-row items-center justify-center border-t border-slate-400 mt-3 pt-3 pb-0">
+        <span className="pb-3 lg:pb-0">Get in touch:</span>
+        <span>
+          {socialLinks.map((socialLink, i) => {
+            return (
+              <Link
+                href={socialLink.url}
+                key={i}
+                className="social-link-dark h-8 w-8 inline-block align-middle text-center aspect-square rounded-full leading-7 mx-3"
+                title={socialLink.name}
+              >
+                <Image
+                  src={`https://cdn.simpleicons.org/${socialLink.icon}`}
+                  alt={socialLink.name}
+                  width={15}
+                  height={15}
+                  className="inline"
+                />
+              </Link>
+            );
+          })}
+          <Link
+            href={`mailto:${CONTACT_EMAIL}`}
+            title="Contact me"
+            className="social-link-dark h-8 w-8 inline-block align-middle text-center aspect-square rounded-full leading-7 mx-3"
+          >
+            <Image
+              src="/envelope-solid.svg"
+              alt="mail"
+              width={15}
+              height={15}
+              className="inline"
+            />
+          </Link>
+        </span>
       </p>
     </section>
   );
